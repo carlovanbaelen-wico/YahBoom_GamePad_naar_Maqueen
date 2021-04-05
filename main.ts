3b936ef4a69428db9b6d517d9768e29af9dd3920
carlovanbaelen-wico <67262093+carlovanbaelen-wico@users.noreply.github.com>
2021-04-05 17:50:07 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,28 @@
+GHBit.onKey(GHBit.enButton.B3, function () {
+    radio.sendNumber(8)
+})
+GHBit.onKey(GHBit.enButton.B1, function () {
+    radio.sendNumber(5)
+})
+GHBit.onKey(GHBit.enButton.B4, function () {
+    radio.sendNumber(7)
+})
+GHBit.onKey(GHBit.enButton.B2, function () {
+    radio.sendNumber(6)
+})
+radio.setGroup(1)
+basic.forever(function () {
+    if (GHBit.Rocker(GHBit.enRocker.Up)) {
+        radio.sendNumber(1)
+    } else if (GHBit.Rocker(GHBit.enRocker.Down)) {
+        radio.sendNumber(2)
+    } else if (GHBit.Rocker(GHBit.enRocker.Left)) {
+        radio.sendNumber(3)
+    } else if (GHBit.Rocker(GHBit.enRocker.Right)) {
+        radio.sendNumber(4)
+    } else if (GHBit.Rocker(GHBit.enRocker.Nostate)) {
+        radio.sendNumber(0)
+    } else if (GHBit.Rocker(GHBit.enRocker.Press)) {
+        radio.sendNumber(9)
+    }
+})
